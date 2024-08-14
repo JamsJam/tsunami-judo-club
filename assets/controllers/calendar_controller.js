d6e08cf8659c3af8a85c71e1ecd25b3da08258b4
JamsJam <jamsjam971@hotmail.com>
2024-08-14 19:42:38 -0400

    mise en place de CD de developpement

--- assets/controllers/calendar_controller.js
+++ assets/controllers/calendar_controller.js
@@ -17,53 +17,31 @@ export default class extends Controller {
         daysInMonth: Number
     };
 
+
     
     async initialize() {
 
 
-        // Extend Day.js with localeData and set the locale to French
-        dayjs.locale(fr);
-        dayjs.extend(localeData, localizedFormat );
+
+        this.initialiseDayjs()
         this.getRightFormat(this.todayValue, 'DD-MM-YYYY')
 
         try {
 
 
             //? ========== initialize component
-            this.component = await getComponent(this.element);
-            console.log('Component successfully initialized:', this.component);
+                this.component = await getComponent(this.element);
+                console.log('Component successfully initialized:', this.component);
+
 
             //? ========== component Event triger
-            this.component.on('model:set', (model, value, component) => {
+                this.setComponentListener(this.component)
                 
-                model =='today' && this.handelTodayChange(value)
-
-            });
-
-
         } catch (error) {
             console.error('Failed to initialize component:', error);
         }
     }
 
-
-    /**
-     * 
-     * @param {String} date date d'aujourd'hui
-     * @param {string} format format de sortie default: DD-MM-YYYY 
-     */
-    getRightFormat(date,format){
-
-        this.todayValue = dayjs(date).format(format = 'DD-MM-YYYY')
-    }
-
-    handelTodayChange(value) {
-        this.todayValue = value
-        this.getRightFormat(value )
-        this.setDayInMonth()
-        this.yearFromToday()
-        this.monthFromToday()
-    }
 
 
     yearsTargetConnected() {
@@ -87,7 +65,7 @@ export default class extends Controller {
     }
 
 
-     connect() {
+    connect() {
         // Calculate and update the number of days in the month
         
         this.setDayInMonth();
@@ -96,91 +74,90 @@ export default class extends Controller {
 
     }
 
+
+
+
+
+    handelTodayChange(value) {
+        this.todayValue = value
+        this.getRightFormat(value )
+        this.setDayInMonth()
+        this.yearFromToday()
+        this.monthFromToday()
+    }
+
+
+
+
+
+
+
     /**
-     * 
-     * @param {dayjs} today 
-     * @returns {Array} days to display before the current days'month
+     * @description Set TwigComponents Eventlistener
+     * @param {*} TwigComponent
+     * @returns {void}
      */
-    getDayBefore(today){
-        const firstDayIndex = dayjs(today).date(1).day()
-        console.log(firstDayIndex)
-        const daysBefore = []
-        if(firstDayIndex !== 0){
+    setComponentListener(TwigComponent)
+    {
 
-            const daysBeforeNumber = firstDayIndex - 1 
+        TwigComponent.on('connect', (component) => {
+
+        });
+
+        TwigComponent.on('disconnect ', (component) => {
+
+        });
+
+
+        //* events are only dispatched when the component is re-rendered (via an action or a model change).
+        TwigComponent.on('render:started', (html, backendResponse, shouldRender= { shouldRender: true}) => {
+
+        });
+
+
+        //* events are only dispatched when the component is re-rendered (via an action or a model change).
+        TwigComponent.on('render:finished', (component) => {
             
+            this.handleReload()
+        });
 
-            for (let index = 0; index < daysBeforeNumber ; index++) {
-                const element = dayjs(today).date(parseInt(`-${index}`));
+
+        TwigComponent.on('loading.state:started', (html, backendRequest) => {
+
+        });
+
+
+        TwigComponent.on('loading.state:finished', (html) => {
+        
+
+        });
+
+
+        TwigComponent.on('model:set', (model, value, component) => {
+
+
+            switch (model) {
                 
-                daysBefore.push({
-                    date: element.format('DD-MM-YYYY'),
-                    id: element.format('DDMMYYYY')
-                })
-            }
-        }
+                case 'today':
 
-        return daysBefore.sort((a, b) => {
-            // Convertir les dates au format JJ-MM-AAAA en objets Date
-            let dateA = new Date(a.date.split('-').reverse().join('-'));
-            let dateB = new Date(b.date.split('-').reverse().join('-'));
+                    this.handelTodayChange(value)
+                    break;
             
-            return dateA - dateB; // Tri ascendant
+                default:
+                    break;
+            }
+
         });
     }
 
-    /**
-     * 
-     * @param {dayjs} today 
-     * @param {int} dayInMonth days in the rcurrent month
-     * @returns {Array} days to display next the current days'month
-     */
-    getDayNext(today,dayInMonth){
-        const lastDayIndex = dayjs(today,).date(dayInMonth).day()
- 
-        const daysNext = []
 
-        if ( lastDayIndex !== 0) {
 
-            const daysNextNumber =  7 - lastDayIndex 
-            
 
-            for (let index = 1; index < daysNextNumber ; index++) {
-                const element = dayjs(today).date(index + dayInMonth);
-                
-                daysNext.push({
-                    date: element.format('DD-MM-YYYY'),
-                    id: element.format('DDMMYYYY')
-                })
-            }
-        }
 
-        return daysNext.sort()
+    handleReload(){
+        this.setDayInMonth()
+        this.component.render()
     }
-    /**
-     * 
-     * @param {dayjs} today today
-     * @param {int} dayInMonth days in the rcurrent month
-     * @returns {Array} days the current days'month
-     * 
-     */
-    getDayCurrent(today, dayInMonth){
-
-        // const firstDayIndex = dayjs(today).date(1).day()
- 
-        const daysCurrent = []
-        
-            for (let index = 1; index <= dayInMonth ; index++) {
-                const element = dayjs(today).date(parseInt(index));
-                
-                daysCurrent.push({
-                    date: element.format('DD-MM-YYYY'),
-                    id: element.format('DDMMYYYY'),
-                })
-            }
-        return daysCurrent
-    }
-
 
 
     setDayInMonth() {
@@ -190,31 +167,128 @@ export default class extends Controller {
     }
 
 
-    handleReload(){
-        
-        this.setDayInMonth()
-        // this.component.set('today','04-02-2000')
-        // this.component.on('model:set'),(model, value, component) => {
-        //     console.log(model, value, component) 
-        // }
-        // this.component.set('dayInMonth',this.daysInMonthValue)
-        // this.daysInMonthValue = dayjs(this.todayValue).daysInMonth()
-        // this.yearFromToday()
-        // this.monthFromToday()
-        this.component.render()
-        console.log(this.todayValue)
-
-    }
-
     yearFromToday(){
         this.yearValue = String(dayjs(this.todayValue).year());
         this.yearsTarget.innerHTML = `<p>${this.yearValue}</p>`;
-        console.log('Year connected:', this.yearValue);
     }
 
     monthFromToday(){
         this.monthValue = dayjs.months()[dayjs(this.todayValue).month()];
         this.monthTarget.innerHTML = `<p>${this.monthValue}</p>`;
-        console.log('Month connected:', this.monthValue);
     }
+
+
+
+
+        /**
+     * 
+     * @param {dayjs} today today
+     * @param {int} dayInMonth days in the rcurrent month
+     * @returns {Array} days the current days'month
+     * 
+     */
+        getDayCurrent(today, dayInMonth){
+
+            // const firstDayIndex = dayjs(today).date(1).day()
+     
+            const daysCurrent = []
+            
+                for (let index = 1; index <= dayInMonth ; index++) {
+                    const element = dayjs(today).date(parseInt(index));
+                    
+                    daysCurrent.push({
+                        date: element.format('DD-MM-YYYY'),
+                        id: element.format('DDMMYYYY'),
+                    })
+                }
+            return daysCurrent
+        }
+    
+        
+        /**
+         * 
+         * @param {dayjs} today 
+         * @returns {Array} days to display before the current days'month
+         */
+        getDayBefore(today){
+            const firstDayIndex = dayjs(today).date(1).day()
+            console.log(firstDayIndex)
+            const daysBefore = []
+            if(firstDayIndex !== 0){
+    
+                const daysBeforeNumber = firstDayIndex - 1 
+                
+    
+                for (let index = 0; index < daysBeforeNumber ; index++) {
+                    const element = dayjs(today).date(parseInt(`-${index}`));
+                    
+                    daysBefore.push({
+                        date: element.format('DD-MM-YYYY'),
+                        id: element.format('DDMMYYYY')
+                    })
+                }
+            }
+    
+            return daysBefore.sort((a, b) => {
+                // Convertir les dates au format JJ-MM-AAAA en objets Date
+                let dateA = new Date(a.date.split('-').reverse().join('-'));
+                let dateB = new Date(b.date.split('-').reverse().join('-'));
+                
+                return dateA - dateB; // Tri ascendant
+            });
+        }
+    
+        /**
+         * 
+         * @param {dayjs} today 
+         * @param {int} dayInMonth days in the rcurrent month
+         * @returns {Array} days to display next the current days'month
+         */
+        getDayNext(today,dayInMonth){
+            const lastDayIndex = dayjs(today,).date(dayInMonth).day()
+     
+            const daysNext = []
+    
+            if ( lastDayIndex !== 0) {
+    
+                const daysNextNumber =  7 - lastDayIndex 
+                
+    
+                for (let index = 1; index < daysNextNumber ; index++) {
+                    const element = dayjs(today).date(index + dayInMonth);
+                    
+                    daysNext.push({
+                        date: element.format('DD-MM-YYYY'),
+                        id: element.format('DDMMYYYY')
+                    })
+                }
+            }
+    
+            return daysNext.sort()
+        }
+
+
+
+
+
+
+    //? helper daysjs
+
+
+        initialiseDayjs(){
+            dayjs.extend(localeData, localizedFormat );
+            dayjs.locale(fr);
+        }
+
+        /**
+         * 
+         * @param {string} date date d'aujourd'hui
+         * @param {string} format format de sortie default: 'DD-MM-YYYY'
+         * @returns {string} date au format donnée
+         */
+        getRightFormat(date,format){
+
+            this.todayValue = dayjs(date).format(format = 'DD-MM-YYYY')
+        }
+
 }
