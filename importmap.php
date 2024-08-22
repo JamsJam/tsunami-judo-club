4ac4818db37780fa5d32c060ddf55097ceb1a1e3
JamsJam <jamsjam971@hotmail.com>
2024-08-22 14:24:37 -0400

    Affichage evenement ponctuel

--- importmap.php
+++ importmap.php
@@ -29,46 +29,46 @@ return [
         'version' => '8.0.5',
     ],
     '@swup/fade-theme' => [
-        'version' => '1.0.5',
+        'version' => '2.0.1',
     ],
     '@swup/slide-theme' => [
-        'version' => '1.0.5',
+        'version' => '2.0.1',
     ],
     '@swup/forms-plugin' => [
-        'version' => '2.0.1',
+        'version' => '3.6.0',
     ],
     '@swup/plugin' => [
         'version' => '4.0.0',
     ],
     'swup' => [
-        'version' => '3.1.1',
+        'version' => '4.7.0',
     ],
     'delegate-it' => [
-        'version' => '6.0.1',
+        'version' => '6.2.0',
     ],
     '@swup/debug-plugin' => [
-        'version' => '3.0.0',
+        'version' => '4.1.0',
     ],
     '@swup/theme' => [
         'version' => '2.1.0',
     ],
     'path-to-regexp' => [
-        'version' => '6.2.2',
+        'version' => '7.1.0',
     ],
     'dayjs' => [
-        'version' => '1.11.12',
+        'version' => '1.11.13',
     ],
     'dayjs/plugin/advancedFormat' => [
-        'version' => '1.11.12',
+        'version' => '1.11.13',
     ],
     'dayjs/plugin/localeData' => [
-        'version' => '1.11.12',
+        'version' => '1.11.13',
     ],
     'dayjs/locale/fr' => [
-        'version' => '1.11.12',
+        'version' => '1.11.13',
     ],
     'dayjs/plugin/localizedFormat' => [
-        'version' => '1.11.12',
+        'version' => '1.11.13',
     ],
     '@swup/head-plugin' => [
         'version' => '2.2.0',
